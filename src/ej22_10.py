c64aa0be00722ec1f9b018260bc873a21e8bf8b9
mgarmorn <mgarmorn@g.educaand.es>
2023-10-30 18:33:14 +0100

    ª

--- src/ej22_10.py
+++ src/ej22_10.py
@@ -2,28 +2,16 @@ from ej22_03 import pedir_numero
 
 def numero_es_primo(numero):
     for i in range(2, numero):
+        
         if (numero % i) == 0:
             return False
+       
         else:
             return True
 
 
-
-    es_primo = True
-    for i in range(2, numero**0.5) + 1):
-        if numero % i == 0:
-            es_primo = False
-            break
-
-    if es_primo:
-        print("El número es primo.")
-    else:
-        print("El número no es primo.")
-
-
-
 def main():
-    numero = pedir_numero
+    numero = pedir_numero()
 
     if numero_es_primo(numero):
         print("El numero es primo")
